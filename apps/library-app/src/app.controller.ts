93a019b422d670b9bee18c5d8e1697e52f7fd82f
Sharan Prasad <sharan.p@DQ27VP9G3Q.local>
2024-07-04 12:53:29 +1200

    init commit

--- apps/library-app/src/app.controller.ts
+++ apps/library-app/src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { add } from '@repo/utils';
 
 @Controller()
 export class AppController {
@@ -7,6 +8,7 @@ export class AppController {
 
   @Get()
   getHello(): string {
+    console.log(add(2, 4));
     return this.appService.getHello();
   }
 }
